78a7712634d986f2c9fccb134902c55d38ba94d4
riothetic <riothetic@gmail.com>
2017-07-27 11:30:14 +0200

    final version

--- FileProcessor.py
+++ FileProcessor.py
@@ -1,8 +1,8 @@
-# filtering videoname and rating
-
 import os, os.path
 
-# counting files in the renamed directory
+
+#----- counting files in the renamed directory -----#
+
 ids = len(os.listdir('./results/renamed/'))
 
 n = 0
@@ -18,10 +18,11 @@ while n < ids:
 print participantid
 
 
-
 def FilterLogs(participantid):
 
-    print('filter...')
+    print('filtering...')
+
+
     for idx in participantid:
         infile = file('./results/renamed/%s.txt' % idx, 'r')
         outfile = file('./results/filtered/%s.txt' % idx, 'w')
@@ -29,42 +30,51 @@ def FilterLogs(participantid):
         ratings = []
         results = []
 
+
         for idx,line in enumerate(infile):
             values = line.strip().split(',')
             videos.append(values[1])
             ratings.append(values[2])
             outfile.write('%s,%s\n' % (videos[idx],ratings[idx]))
 
+
     print('filtering successful')
 
 
-# sorting the files by videoname
+#----- sorting the files by videoname -----#
 
 def SortLogs(participantid):
-    
+
     print('sorting...')
+
+
     for num in participantid:
         infile = file('./results/filtered/%s.txt' % num, 'r')
         outfile = file('./results/sorted/%s.txt' % num, 'w')
 
         xlist = []
 
+
         for line in infile:
             xlist.append(line)
 
-        xlist.pop(0)
 
+        xlist.pop(0)
         sortedxlist = sorted(xlist)
+
 
         for idx, element in enumerate(sortedxlist):
             outfile.write(sortedxlist[idx])
 
+
     print('sorting successful')
 
 
+#----- joining the actions (2 separate files before) -----#
 
 def master():
     FilterLogs(participantid)
     SortLogs(participantid)
+
 
 master()
--- ResultSpreadsheet.py
+++ ResultSpreadsheet.py
@@ -3,22 +3,22 @@
 import numpy as np
 import csv
 import os, os.path
+import matplotlib.pyplot as plt
 
 
 #----- declaring variables/lists -----#
 
-main_list = []                                                                          # list of lists with all the videoratings per person
-#rating_list = []                                                                       # variable for the lists inside main_list
-name_list = []                                                                          # list with all the videonames
-mean_list = []
-participant_mean_list = []
-video_mean_list = []
-n = 0
-participant_id_list = []                                                                      # list of partipant id's
+main_list = []                  # list of lists with all the videoratings per person((4,5,2,4,...)(3,4,2,5,...))
+name_list = []                  # list with all the videonames (video_hc023.mp4, ...)
+participant_mean_list = []      # arithmetic mean per participant
+video_mean_list = []            # arithmetic mean per video
+participant_id_list = []        # list of partipant id's (11,12,13,14,...)
+
 c = 0
+n = 0
 
 
-#----- counting files and makin a list of filenumbers (participant-id's) -----#
+#----- counting files and making a list of filenumbers (participant-id's) -----#
 
 ids = len(os.listdir('./results/sorted/'))
 
@@ -34,8 +34,6 @@ for f in participant_id_list:
 
     infile = file('./results/sorted/%s.txt' % f, 'r')
     tmp_rating_list = []
-
-
     c = c + 1
 
 
@@ -67,6 +65,7 @@ for f in participant_id_list:
 #----- calculating mean per person -----#
 
 for tmp_rating_person_list in main_list:
+
     tmp_rating = np.array(tmp_rating_person_list).astype(np.float)
     arithm = np.mean(tmp_rating)
     participant_mean_list.append(arithm)
@@ -96,7 +95,6 @@ with open('./results/Spreadsheet_videoname.csv', 'w') as spreadsheet_file:
     spreadsheet_writer.writerow(['Videoname'] + ['Mean'] + participant_id_list)
 
 
-
     for idx,e in enumerate(name_list):
 
         tmp_video_rating_list = [value[idx] for value in main_list]
@@ -111,65 +109,9 @@ with open('./results/Spreadsheet_person.csv', 'w') as spreadsheet_file:
     spreadsheet_writer.writerow(['Participant ID'] + ['Mean'] + ['Correlation'])
 
 
-
     for idx,e in enumerate(participant_id_list):
 
         corrx = main_list[idx]
         corry = video_mean_list
         corr = np.corrcoef(corrx, corry)
         spreadsheet_writer.writerow([e] + [participant_mean_list[idx]] + [corr[0][1]])
-
-
-'''
-
-#----- writing the video names into the first csv column -----#
-
-main_outfile.write('Videoname\n')
-for e in name_list:
-    main_outfile.write('%s\n' % e)
-
-
-
-
-
-with open('./results/Spreadsheet.csv', 'rw') as file:
-
-    file.write()
-
-    for idx,row in enumerate(reader):
-        if row[0] == 'Videoname':
-            pass
-        else:
-            writer.writerow(row+[videoname[idx -1]])
-
-
-
-
-    #for name in name_list:
-        #writer.writerow(row + name)
-
-
-#----- writing the overall mean per video into the second csv column-----#
-
-with open('./results/Spreadsheet.csv', 'rw') as file:
-
-    reader = csv.reader(file)
-    writer = csv.writer(file, delimiter=',')
-
-    for idx,row in enumerate(reader):
-
-        if row[0] == 'Videoname':
-            writer.writerow(row+['Mean'])
-        else:
-            rating_list = np.array(main_list[idx -1]).astype(np.float)
-            arithm = np.mean(rating_list)
-            writer.writerow(row+[arithm])
-
-
-    for rating_list in main_list:
-        rating_list = np.array(rating_list).astype(np.float)
-        arithm = np.mean(rating_list)
-        print(arithm)
-        writer.writerow(arithm)
-
-'''
